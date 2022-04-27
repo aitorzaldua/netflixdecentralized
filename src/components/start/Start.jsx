f78120f5fdccbea48552a755f43ad29047c8fa90
Aitor Zaldua <aitor.zaldua@draftdigital.org>
2022-04-27 16:23:31 +0200

    270422 CSS Connect Wallet

--- src/components/start/Start.jsx
+++ src/components/start/Start.jsx
@@ -1,13 +1,26 @@
 import React from 'react';
 import './start.css';
 import bg from '../../images/netflixStart.jpeg';
+import startLogo from '../../images/Netflix_logo_PNG1.png';
+
+import {ConnectButton } from 'web3uikit';
+import { useMoralis  } from 'react-moralis';
 
 const Start = () => {
   return (
     <>
     <div className="container" style={{ backgroundImage: `url(${bg})` }}>
-        <div className='containerGradient'></div>
-        <div className='frontText'>Unlimited movies, TV shows, and more.</div>
+        <div className='containerGradient'>
+          <div className='start-navBar'>
+            <img className='startLogo' src = {startLogo} alt = 'netflix_logo' />
+            <ConnectButton />
+          </div>
+          <div className='our-story-card-text'>
+            <h1 className='our-story-card-title'>Unlimited movies, TV shows, and more.</h1>
+            <h2 className='our-story-card-subtitle'>Watch anywhere. Cancel anytime. </h2>
+            <h3 className='wallet-title'>Ready to watch? Connect your wallet to subscribe or restart your membership.</h3>
+          </div>
+        </div>
     </div>
 
     </>
