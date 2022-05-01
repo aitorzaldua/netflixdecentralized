0d6c15a7c0e59edbfb444662cc874821cd75a131
Aitor Zaldua <aitor.zaldua@draftdigital.org>
2022-05-01 21:26:21 +0200

    010522 trying moralis card

--- src/components/subscription/Subscription.jsx
+++ src/components/subscription/Subscription.jsx
@@ -3,11 +3,12 @@ import './subscription.css';
 import startLogo from '../../images/Netflix_logo_PNG1.png';
 import polygonLogo from '../../images/polygon-matic-logo.png'
 
+import {Icon, Card  } from 'web3uikit';
+
 const Subscription = () => {
   return (
     <div className='gradient-background'>
         <img className='startLogo' src = {startLogo} alt = 'netflix_logo' />
-        
         <div className='gradient-background-text'>
             <h1 className='gradient-background-title'>You´re only one step away from streaming.</h1>
             <div className='gradient-background-subtitle'>
@@ -17,7 +18,24 @@ const Subscription = () => {
             </div>
             <h3 className='gradient-background-wallet'>Ready to watch? Connect your wallet to subscribe or restart your membership.</h3>
         </div>
+        <div className='container__subs'>
+            <div className='about__cards'>
+                <article className='about__card'>
+                    <h5>Experience</h5>
+                    <small>24+ Years Working</small>
+                </article>
+
+                <article className='about__card'>
+                    <h5>Clients</h5>
+                    <small>100+ Worldwide</small>
+                </article>
+            </div>
+        </div>
+
+        <Card onClick={null} setIsSelected={null}  />
+
     </div>
+
   )
 }
 
